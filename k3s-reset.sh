af8dd5e9f08b0517a07be4c5e70bc0da72ed7773
Jamie Sammons <jamie.sammons@liferay.com>
2025-06-25 20:21:43 -0500

    Add cloudnative-pg controller

--- k3s-reset.sh
+++ k3s-reset.sh
@@ -2,6 +2,8 @@
 
 /usr/local/bin/k3s-uninstall.sh
 
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" sh -
+sudo rm -fr /home/me/.rancher/k3s/
+
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" sh -s - --data-dir /home/me/.rancher/k3s
 
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
