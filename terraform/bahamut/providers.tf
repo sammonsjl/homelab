66a0991d4d6e382b92049c7ff3b0b9da2bde35af
Jamie Sammons <sammonsjl@gmail.com>
2025-10-22 16:16:27 -0500

    terraform: automate k3d and fluxcd

---
+++ terraform/bahamut/providers.tf
@@ -0,0 +1,18 @@
+provider "flux" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+  git = {
+    branch = "k3d-base"
+    url    = "https://github.com/${var.github_org}/${var.github_repository}.git"
+    http = {
+      username = "git"
+      password = var.github_token
+    }
+  }
+}
+
+provider "github" {
+  owner = var.github_org
+  token = var.github_token
+}
