8e9b1a9deb4fff5853beeaf380b4fc2622791673
Jamie Sammons <sammonsjl@gmail.com>
2025-10-30 22:36:56 -0500

    terraform: deploy cilium

--- terraform/bahamut/main.tf
+++ terraform/bahamut/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.0"
+    }
     docker = {
       source  = "kreuzwerker/docker"
       version = "3.8.0"
@@ -32,21 +36,67 @@ resource "docker_network" "cilium_network" {
 }
 
 resource "k3d_cluster" "bahamut" {
-  name    = "bahamut"
-  servers = 1
+  depends_on = [docker_network.cilium_network]
+  name       = "bahamut"
+  servers    = 1
+  agents     = 3
+
+  kube_api {
+    host_ip   = "172.50.0.1"
+    host_port = 6445
+  }
+
+  image   = "rancher/k3s:v1.34.1-k3s1"
+  network = "cilium"
+  token   = "Ciliumk3d1"
+
+  volume {
+    source      = "/home/jamie/projects/homelab/terraform/bahamut/k3d-entrypoint-cilium.sh"
+    destination = "/bin/k3d-entrypoint-cilium.sh"
+    node_filters = [
+      "all"
+    ]
+  }
+
+  k3d {
+    disable_load_balancer = true
+    disable_image_volume  = false
+  }
+
+  k3s {
+    dynamic "extra_args" {
+      for_each = [
+        "--tls-san=127.0.0.1",
+        "--disable=servicelb",
+        "--disable=traefik",
+        "--disable-network-policy",
+        "--flannel-backend=none",
+        "--disable=kube-proxy",
+        "--cluster-cidr=10.21.0.0/16",
+        "--service-cidr=10.201.0.0/16"
+      ]
+      content {
+        arg          = extra_args.value
+        node_filters = ["server:*"]
+      }
+    }
+  }
 
   kubeconfig {
     update_default_kubeconfig = true
     switch_current_context    = true
   }
+}
 
-  port {
-    host_port      = 80
-    container_port = 80
-    node_filters = [
-      "loadbalancer",
-    ]
-  }
+resource "helm_release" "cilium" {
+  depends_on = [k3d_cluster.bahamut]
+  name       = "cilium"
+  namespace  = "kube-system"
+  repository = "https://helm.cilium.io/"
+  chart      = "cilium"
+  version    = "1.18.3"
+  values     = [file("../../infrastructure/controllers/bahamut/cilium/values.yaml")]
+  wait       = false
 }
 
 resource "flux_bootstrap_git" "this" {
